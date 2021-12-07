58268eaa139099728657f35a5f8e3026d6b0de91
Omkar Deshmukh <omdesh.0407@gmail.com>
2021-12-07 14:55:33 +0530

    Dec2021 Day1

---
+++ Dec2021/Day1/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace Day1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Read the file input.txt
+            string[] lines = System.IO.File.ReadAllLines(@"input.txt");
+            Part1(lines);
+            Console.Write(Part2(lines));
+        }
+
+        static int Part1(string[] input)
+        {
+
+
+
+            if (input.Length > 0)
+            {
+
+
+                int measurementsLargerThanPreviousCount = 0;
+                int previousMeasurement = Int32.Parse(input[0]);
+                for (int i = 1; i < input.Length; i++)
+                {
+                    int currentMeasurement = Int32.Parse(input[i]);
+                    if (currentMeasurement > previousMeasurement)
+                    {
+                        measurementsLargerThanPreviousCount++;
+                    }
+                    previousMeasurement = currentMeasurement;
+                }
+
+                return measurementsLargerThanPreviousCount;
+            }
+            return -1;
+        }
+
+        static int Part2(string[] input)
+        {
+            if (input.Length > 0)
+            {
+                int measurementsLargerThanPreviousCount = 0;
+                int previousMeasurement = Int32.Parse(input[0]) + Int32.Parse(input[1]) + Int32.Parse(input[2]);
+                for (int i = 1; i < input.Length-2; i++)
+                {
+                    int currentMeasurement = Int32.Parse(input[i]) + Int32.Parse(input[i + 1]) + Int32.Parse(input[i + 2]);
+                    if (currentMeasurement > previousMeasurement)
+                    {
+                        measurementsLargerThanPreviousCount++;
+                    }
+                    previousMeasurement = currentMeasurement;
+
+                }
+                return measurementsLargerThanPreviousCount;
+
+            }
+            return -1;
+        }
+    }
+}
